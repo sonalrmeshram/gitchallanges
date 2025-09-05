7093510e3fb792366389bc00b6ccd3fa32d6f4d0
sonal <sonalrmeshram@hmail.com>
2025-09-05 11:45:06 +0530

    test.tf

---
+++ test.tf
@@ -0,0 +1,14 @@
+resource "aws_instance" "name" {
+ami = "ami-084a7d336e816906b"
+instance_type ="t2.micro" 
+availability_zone = "us-east-1a"
+tags = {
+  Name = "test"
+}
+
+    lifecycle {
+      prevent_destroy  = true
+      create_before_destroy = true
+      ignore_changes = [ tags, ]
+    }
+}
